3541ac1f65af09edc66c640360fa1d5e147ce14a
Shocker <zacsmcdowell@gmail.com>
2023-11-03 16:25:52 -0400

    adding base structure and packages, added first model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const userData = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }]
+},
+{
+    toJSON: {
+        virtuals: true,
+    }
+})
+
+userData.virtual("friendcount").get(function(){
+    return this.friends.length
+})
+
+const User = model("User", userData)
+module.exports = User
